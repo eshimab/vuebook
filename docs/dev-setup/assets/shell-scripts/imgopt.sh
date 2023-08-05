071e0e44976b1c3f8d05f3a02607ffededd99f67
Eric <eric21@hawaii.edu>
2023-08-04 14:42:30 -1000

    minor update to imgopt

--- docs/dev-setup/assets/shell-scripts/imgopt.sh
+++ docs/dev-setup/assets/shell-scripts/imgopt.sh
@@ -7,7 +7,7 @@
 
 
 # Specify the input directory containing HEIC files
-input_dir="/Users/eshim/vuebook/docs/influx-flight/flight-begin/assets/img-power"
+# input_dir="/Users/eshim/vuebook/docs/influx-flight/flight-begin/assets/img-power"
 input_dir=$(pwd)
 output_dir="${input_dir}"
 # Create directories for saving converted images
@@ -44,12 +44,23 @@ done
 
 
 
+# ------------------ CONVERTING PNGS
 # Loop through PNG files in the input directory
 input_dir=$(pwd)
 output_dir="${input_dir}"
 # Create directories for saving converted images
 orig_dir="${input_dir}/origimg"
 mkdir -p "${orig_dir}"
+
+# Extract the relevant parts of the current directory
+# The /Users/username/vuebook part
+project_dir=$(echo "$input_dir" | cut -d'/' -f1-4)
+# The /Users/username/vuebook/oversized-assets
+oversized_assets_dir="$project_dir/oversized-assets"
+# The target dir with filestructure in oversized-assets
+target_dir="$oversized_assets_dir${input_dir#$project_dir}/origimg"
+mkdir -p "${target_dir}"
+# Loop through PNGs
 for file in "$input_dir"/*.png; do
     # Get Filename
     filename=$(basename "$file")
@@ -59,15 +70,6 @@ for file in "$input_dir"/*.png; do
     # Apply ImageMagick 
     convert "$file" -resize 720x -colors 256 "$file"
 done
-# Extract the relevant parts of the current directory
-# The /Users/username/vuebook part
-project_dir=$(echo "$input_dir" | cut -d'/' -f1-4)
-# The /Users/username/vuebook/oversized-assets
-oversized_assets_dir="$project_dir/oversized-assets"
-# The /Users/username/vuebook/oversized-assets/docs/
-# target_dir="$oversized_assets_dir${current_dir#$project_dir}/origimg"
-target_dir="$oversized_assets_dir${input_dir#$project_dir}/origimg"
-mkdir -p "${target_dir}"
 
 # Sync Files
 orig_all_files="${orig_dir}/*"
